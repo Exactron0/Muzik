140979be076be1c886c5e2d5660d910633cc4252
๖ۣۜExactrøn. <Exactron0@gmail.com>
2024-12-22 15:25:33 +0300

    Update skip.py

--- WinxMusic/plugins/admins/skip.py
+++ WinxMusic/plugins/admins/skip.py
@@ -36,11 +36,6 @@ async def skip(_client: Client, message: Message, _, chat_id: int):
                             popped = None
                             try:
                                 popped = check.pop(0)
-                                if popped.get("mystic"):
-                                    try:
-                                        await popped.get("mystic").delete()
-                                    except Exception:
-                                        pass
                             except Exception:
                                 return await message.reply_text(_["admin_16"])
                             if popped:
